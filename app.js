1b3733ae6d33af977f332b161a559018587afe03
Christopher <csbroox@gmail.com>
2023-08-22 06:18:52 -0500

    missing an html element

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const h1 = document.querySelector('h1');
-h1.innerHTML = 'something else';
+const ul = document.querySelector('ul');
+const data = [1, 2, 3];
+
+// function render(){
+//   const html = data.map(function(num){
+//     return `<li>${num}</li>`;
+//   });
+//   ul.innerHTML = html.join('');
+// }
+
+// render();
+
+setInterval(function(){
+  const rnd = Math.ceil(Math.random() * 10);
+ // console.log(rnd);
+  data.push(rnd);
+}, 1000);
