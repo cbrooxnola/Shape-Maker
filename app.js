7fa189e597f3244ce3154d81d578884cebe4c8b4
Christopher <csbroox@gmail.com>
2023-08-22 07:25:24 -0500

    pre-break commit

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const ul = document.querySelector('ul');
-const data = [1, 2, 3];
+// const ul = document.querySelector('ul');
+// const data = [1, 2, 3];
 
-function render(){
-  const html = data.map(function(num){
-    return `<li>${num}</li>`;
-  });
-  ul.innerHTML = html.join('');
+// function render(){
+//   const html = data.map(function(num){
+//     return `<li>${num}</li>`;
+//   });
+//   ul.innerHTML = html.join('');
+// }
+
+// render();
+
+// const i = setInterval(function(){
+//   const rnd = Math.ceil(Math.random() * 10);
+//   //console.log(rnd);
+//   data.push(rnd);
+//   render();
+//   if (data.length === 10){
+//     clearInterval(i);
+//   }
+// }, 1000);
+const container = document.querySelector('.container');
+const shapes = [
+{
+  color: 'red',
+  size: 'md'
+},
+{
+  color: 'green',
+  size: 'lg'
 }
-
+];
+function render(){
+  const html = shapes.map(function(shape){
+    return `<div> class='${shape.color} ${shape.size}'</div>`;
+  });
+  container.HTML = html.join('');
+}
 render();
-
-const i = setInterval(function(){
-  const rnd = Math.ceil(Math.random() * 10);
-  console.log(rnd);
-  data.push(rnd);
-  render();
-  if (data.length === 10){
-    clearInterval(i);
-  }
-}, 1000);
