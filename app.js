916a68c1d64916364056f220df50b7e683d97210
Christopher <csbroox@gmail.com>
2023-08-22 06:38:27 -0500

    fixed html

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const ul = document.querySelector('ul');
 const data = [1, 2, 3];
 
-// function render(){
-//   const html = data.map(function(num){
-//     return `<li>${num}</li>`;
-//   });
-//   ul.innerHTML = html.join('');
-// }
+function render(){
+  const html = data.map(function(num){
+    return `<li>${num}</li>`;
+  });
+  ul.innerHTML = html.join('');
+}
 
-// render();
+render();
 
-setInterval(function(){
+const i = setInterval(function(){
   const rnd = Math.ceil(Math.random() * 10);
- // console.log(rnd);
+  console.log(rnd);
   data.push(rnd);
+  render();
+  if (data.length === 10){
+    clearInterval(i);
+  }
 }, 1000);
